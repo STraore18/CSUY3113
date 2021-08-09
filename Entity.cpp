eff71ffda48414d8dcf24c675a9c7e18191a6188
STraore18 <47250690+STraore18@users.noreply.github.com>
2021-08-08 23:32:24 -0400

    Add files via upload

---
+++ Entity.cpp
@@ -0,0 +1,36 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+    position = glm::vec3(0);
+    acceleration = glm::vec3(0);
+    velocity = glm::vec3(0);
+    rotation = glm::vec3(0);
+
+    modelMatrix = glm::mat4(1.0f);
+    
+    speed = 0.0f;
+}
+
+void Entity::Update(float deltaTime)
+{
+    velocity += acceleration * deltaTime;
+    position += velocity * deltaTime;
+    
+    if (entityType==CUBE){
+        rotation.x += 45 * deltaTime;
+    }
+
+    modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate(modelMatrix, position);
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+}
+
+void Entity::Render(ShaderProgram *program) {
+    program->SetModelMatrix(modelMatrix);
+    
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    mesh->Render(program);
+}
+
